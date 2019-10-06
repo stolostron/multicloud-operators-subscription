c1ce27dbdba990e4231b1ea2043b43ed26326029
Kuan Feng <31184439+kuanf@users.noreply.github.com>
2019-10-06 08:46:57 -0400

    initial check in with CRD only (#2)

---
+++ Makefile
@@ -0,0 +1,91 @@
+# Copyright 2019 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+GIT_HOST = github.com/IBM
+PWD := $(shell pwd)
+BASE_DIR := $(shell basename $(PWD))
+
+# Keep an existing GOPATH, make a private one if it is undefined
+GOPATH_DEFAULT := $(PWD)/.go
+export GOPATH ?= $(GOPATH_DEFAULT)
+GOBIN_DEFAULT := $(GOPATH)/bin
+export GOBIN ?= $(GOBIN_DEFAULT)
+TESTARGS_DEFAULT := "-v"
+export TESTARGS ?= $(TESTARGS_DEFAULT)
+DEST := $(GOPATH)/src/$(GIT_HOST)/$(BASE_DIR)
+VERSION ?= $(shell git describe --exact-match 2> /dev/null || \
+                 git describe --match=$(git rev-parse --short=8 HEAD) --always --dirty --abbrev=8)
+
+# Image URL to use all building/pushing image targets
+IMG ?= multicloud-operators-deployable
+REGISTRY ?= quay.io/multicloudlab
+
+.PHONY: all
+all: check test build images
+
+ifneq ("$(realpath $(DEST))", "$(realpath $(PWD))")
+	$(error Please run 'make' from $(DEST). Current directory is $(PWD))
+endif
+
+include common/Makefile.common.mk
+
+
+############################################################
+# work section
+############################################################
+$(GOBIN):
+	@echo "create gobin"
+	@mkdir -p $(GOBIN)
+
+work: $(GOBIN)
+
+############################################################
+# check section
+############################################################
+check: fmt lint
+
+fmt: format-go format-protos format-python
+
+lint: lint-all
+
+############################################################
+# test section
+############################################################
+
+test:
+	@go test ${TESTARGS} ./...
+
+############################################################
+# build section
+############################################################
+
+build:
+	@common/scripts/gobuild.sh $(IMG) ./cmd
+
+############################################################
+# images section
+############################################################
+
+images: build build-push-images
+
+build-push-images: config-docker
+	@docker build . -f Dockerfile -t $(REGISTRY)/$(IMG):$(VERSION)
+	@docker push $(REGISTRY)/$(IMG):$(VERSION)
+
+############################################################
+# clean section
+############################################################
+clean:
+	rm -f go-repo-template
---
+++ pkg/controller/subscription/subscription_controller.go
@@ -0,0 +1,80 @@
+// Copyright 2019 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package subscription
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	appv1alpha1 "github.com/IBM/multicloud-operators-subscription/pkg/apis/app/v1alpha1"
+)
+
+/**
+* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
+* business logic.  Delete these comments after modifying this file.*
+ */
+
+// Add creates a new Subscription Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileSubscription{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("subscription-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource Subscription
+	err = c.Watch(&source.Kind{Type: &appv1alpha1.Subscription{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// blank assignment to verify that ReconcileSubscription implements reconcile.Reconciler
+var _ reconcile.Reconciler = &ReconcileSubscription{}
+
+// ReconcileSubscription reconciles a Subscription object
+type ReconcileSubscription struct {
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	client client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a Subscription object and makes changes based on the state read
+// and what is in the Subscription.Spec
+func (r *ReconcileSubscription) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	klog.Info("Reconciling: ", request.NamespacedName)
+
+	return reconcile.Result{}, nil
+}
