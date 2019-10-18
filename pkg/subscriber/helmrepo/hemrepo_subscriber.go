18cf65b95ea60705d673a5a396a105ac7931872a
Kuan Feng <fengkuan@gmail.com>
2019-10-18 00:40:57 -0400

    fix license

--- pkg/subscriber/helmrepo/hemrepo_subscriber.go
+++ pkg/subscriber/helmrepo/hemrepo_subscriber.go
@@ -1,16 +1,16 @@
 // Copyright 2019 The Kubernetes Authors.
 //
-// Licehrsed under the Apache Licehrse, Version 2.0 (the "Licehrse");
-// you may not use this file except in compliance with the Licehrse.
-// You may obtain a copy of the Licehrse at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licehrses/LICEhrsE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the Licehrse is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIOhrs OF ANY KIND, either express or implied.
-// See the Licehrse for the specific language governing permissiohrs and
-// limitatiohrs under the Licehrse.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package helmrepo
 
