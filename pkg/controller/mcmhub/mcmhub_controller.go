f747ad5667f22528ede22f2a90e85a602c67a766
Kuan Feng <fengkuan@gmail.com>
2019-10-20 22:14:45 -0400

    add hub controller

---
+++ pkg/controller/mcmhub/mcmhub_controller.go
@@ -0,0 +1,158 @@
+// Copyright 2019 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mcmhub
+
+import (
+	"context"
+	"reflect"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	appv1alpha1 "github.com/IBM/multicloud-operators-subscription/pkg/apis/app/v1alpha1"
+	"github.com/IBM/multicloud-operators-subscription/pkg/utils"
+)
+
+/**
+* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
+* business logic.  Delete these comments after modifying this file.*
+ */
+
+// Add creates a new Subscription Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	rec := &ReconcileSubscription{
+		Client: mgr.GetClient(),
+		scheme: mgr.GetScheme(),
+	}
+
+	return rec
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("mcmhub-subscription-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource Subscription
+	err = c.Watch(&source.Kind{Type: &appv1alpha1.Subscription{}}, &handler.EnqueueRequestForObject{}, utils.SubscriptionPredicateFunctions)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// blank assignment to verify that ReconcileSubscription implements reconcile.Reconciler
+var _ reconcile.Reconciler = &ReconcileSubscription{}
+
+// ReconcileSubscription reconciles a Subscription object
+type ReconcileSubscription struct {
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a Subscription object and makes changes based on the state read
+// and what is in the Subscription.Spec
+func (r *ReconcileSubscription) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	klog.Info("MCM Hub Reconciling subscription: ", request.NamespacedName)
+
+	instance := &appv1alpha1.Subscription{}
+	err := r.Get(context.TODO(), request.NamespacedName, instance)
+
+	if err != nil {
+		if errors.IsNotFound(err) {
+			klog.Info("Subscription: ", request.NamespacedName, " is gone")
+			// Object not found, delete existing subscriberitem if any
+
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	// save original status
+	orgst := instance.Status.DeepCopy()
+
+	// process as hub subscription, generate deployable to propagate
+	pl := instance.Spec.Placement
+	if pl != nil && (pl.PlacementRef != nil || pl.Clusters != nil || pl.ClusterSelector != nil) {
+		err = r.doMCMHubReconcile(instance)
+
+		instance.Status.Phase = appv1alpha1.SubscriptionPropagated
+		if err != nil {
+			instance.Status.Phase = appv1alpha1.SubscriptionFailed
+			instance.Status.Reason = err.Error()
+		}
+	} else {
+		// no longer hub subscription
+		if instance.Status.Phase != appv1alpha1.SubscriptionFailed && instance.Status.Phase != appv1alpha1.SubscriptionSubscribed {
+			instance.Status.Phase = ""
+			instance.Status.Message = ""
+			instance.Status.Reason = ""
+		}
+
+		if instance.Status.Statuses != nil {
+			localkey := types.NamespacedName{}.String()
+			for k := range instance.Status.Statuses {
+				if k != localkey {
+					delete(instance.Status.Statuses, k)
+				}
+			}
+		}
+	}
+
+	result := reconcile.Result{}
+
+	if !reflect.DeepEqual(*orgst, instance.Status) {
+		klog.Info("MCM Hub updating subscriptoin status to ", instance.Status)
+
+		instance.Status.LastUpdateTime = metav1.Now()
+
+		err = r.Status().Update(context.TODO(), instance)
+
+		if err != nil {
+			klog.Error("Failed to update status for mcm hub subscription ", request.NamespacedName, " with error: ", err, " retry after 1 seconds")
+
+			result.RequeueAfter = 1 * time.Second
+		}
+	}
+
+	return result, nil
+}
+
+func (r *ReconcileSubscription) doMCMHubReconcile(instance *appv1alpha1.Subscription) error {
+	return nil
+}
---
+++ pkg/controller/mcmhub/mcmhub_controller_test.go
@@ -0,0 +1,145 @@
+// Copyright 2019 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mcmhub
+
+import (
+	"testing"
+	"time"
+
+	"github.com/onsi/gomega"
+	"golang.org/x/net/context"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	chnv1alpha1 "github.com/IBM/multicloud-operators-channel/pkg/apis/app/v1alpha1"
+	appv1alpha1 "github.com/IBM/multicloud-operators-subscription/pkg/apis/app/v1alpha1"
+)
+
+var c client.Client
+
+var (
+	chnkey = types.NamespacedName{
+		Name:      "test-chn",
+		Namespace: "test-chn-namespace",
+	}
+
+	channel = &chnv1alpha1.Channel{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      chnkey.Name,
+			Namespace: chnkey.Namespace,
+		},
+		Spec: chnv1alpha1.ChannelSpec{
+			Type: chnv1alpha1.ChannelTypeNamespace,
+		},
+	}
+)
+
+var (
+	subkey = types.NamespacedName{
+		Name:      "test-sub",
+		Namespace: "test-sub-namespace",
+	}
+
+	subRef = &corev1.LocalObjectReference{
+		Name: subkey.Name,
+	}
+
+	subscription = &appv1alpha1.Subscription{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      subkey.Name,
+			Namespace: subkey.Namespace,
+		},
+		Spec: appv1alpha1.SubscriptionSpec{
+			Channel: chnkey.String(),
+			PackageFilter: &appv1alpha1.PackageFilter{
+				FilterRef: subRef,
+			},
+		},
+	}
+)
+
+var expectedRequest = reconcile.Request{NamespacedName: subkey}
+
+const timeout = time.Second * 2
+
+func TestReconcile(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
+	// channel when it is finished.
+	mgr, err := manager.New(cfg, manager.Options{})
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+
+	c = mgr.GetClient()
+
+	rec := newReconciler(mgr)
+	recFn, requests := SetupTestReconcile(rec)
+
+	g.Expect(add(mgr, recFn)).NotTo(gomega.HaveOccurred())
+
+	stopMgr, mgrStopped := StartTestManager(mgr, g)
+
+	defer func() {
+		close(stopMgr)
+		mgrStopped.Wait()
+	}()
+
+	chn := channel.DeepCopy()
+	chn.Spec.SecretRef = nil
+	chn.Spec.ConfigMapRef = nil
+	g.Expect(c.Create(context.TODO(), chn)).NotTo(gomega.HaveOccurred())
+
+	defer c.Delete(context.TODO(), chn)
+
+	// Create the Subscription object and expect the Reconcile and Deployment to be created
+	instance := subscription.DeepCopy()
+	instance.Spec.PackageFilter = nil
+	g.Expect(c.Create(context.TODO(), instance)).NotTo(gomega.HaveOccurred())
+
+	defer c.Delete(context.TODO(), instance)
+
+	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
+}
+
+func TestDoMCMReconcile(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	instance := subscription.DeepCopy()
+
+	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
+	// channel when it is finished.
+	mgr, err := manager.New(cfg, manager.Options{})
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+
+	c = mgr.GetClient()
+
+	stopMgr, mgrStopped := StartTestManager(mgr, g)
+
+	defer func() {
+		close(stopMgr)
+		mgrStopped.Wait()
+	}()
+
+	rec := newReconciler(mgr).(*ReconcileSubscription)
+
+	g.Expect(c.Create(context.TODO(), instance)).NotTo(gomega.HaveOccurred())
+
+	defer c.Delete(context.TODO(), instance)
+
+	g.Expect(rec.doMCMHubReconcile(instance)).NotTo(gomega.HaveOccurred())
+}
