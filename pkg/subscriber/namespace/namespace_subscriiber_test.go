62780753681166060f2991ba94b597e215e95b37
Kuan Feng <fengkuan@gmail.com>
2019-11-04 11:00:17 -0500

    clusterscope subscription deploys template to its namespace
    Signed-off-by: Kuan Feng <fengkuan@gmail.com>

--- pkg/subscriber/namespace/namespace_subscriiber_test.go
+++ pkg/subscriber/namespace/namespace_subscriiber_test.go
@@ -118,3 +118,76 @@ func TestDefaultSubscriber(t *testing.T) {
 
 	c.Delete(context.TODO(), ns)
 }
+
+var (
+	defaultworkloadkey = types.NamespacedName{
+		Name:      "testworkload",
+		Namespace: "default",
+	}
+
+	defaultworkloadconfigmap = corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      defaultworkloadkey.Name,
+			Namespace: defaultworkloadkey.Namespace,
+		},
+	}
+
+	defaultchdpl = &dplv1alpha1.Deployable{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployable",
+			APIVersion: "app.ibm.com/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "chdpl",
+			Namespace: id.Namespace,
+		},
+		Spec: dplv1alpha1.DeployableSpec{
+			Template: &runtime.RawExtension{
+				Object: &defaultworkloadconfigmap,
+			},
+		},
+	}
+)
+
+func TestDefaultSubscriberWithClusterScope(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	mgr, err := manager.New(cfg, manager.Options{})
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+
+	c = mgr.GetClient()
+	// prepare default channel
+	ns := chns.DeepCopy()
+	dpl := defaultchdpl.DeepCopy()
+
+	g.Expect(c.Create(context.TODO(), ns)).NotTo(gomega.HaveOccurred())
+	g.Expect(c.Create(context.TODO(), dpl)).NotTo(gomega.HaveOccurred())
+
+	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
+	// channel when it is finished.
+	g.Expect(Add(mgr, cfg, &id, 2)).NotTo(gomega.HaveOccurred())
+	stopMgr, mgrStopped := StartTestManager(mgr, g)
+
+	defer func() {
+		close(stopMgr)
+		mgrStopped.Wait()
+	}()
+
+	cfgmap := &corev1.ConfigMap{}
+
+	time.Sleep(10 * time.Second)
+
+	g.Expect(c.Get(context.TODO(), defaultworkloadkey, cfgmap)).NotTo(gomega.HaveOccurred())
+
+	// clean up
+	c.Delete(context.TODO(), dpl)
+
+	time.Sleep(10 * time.Second)
+	g.Expect(c.Get(context.TODO(), defaultworkloadkey, cfgmap)).To(gomega.HaveOccurred())
+
+	c.Delete(context.TODO(), ns)
+}
