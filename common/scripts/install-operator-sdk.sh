4a573b2c9862f71d57d167db3f090fb5d21416c3
Kuan Feng <fengkuan@gmail.com>
2019-11-05 23:57:51 -0500

    use 0.12.0 to build
    Signed-off-by: Kuan Feng <fengkuan@gmail.com>

--- common/scripts/install-operator-sdk.sh
+++ common/scripts/install-operator-sdk.sh
@@ -17,7 +17,7 @@
 echo ">>> Installing Operator SDK"
 
 # Use version 0.10.0
-RELEASE_VERSION=v0.10.0
+RELEASE_VERSION=v0.12.0
 # Download binary
 curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
 # Install binary
