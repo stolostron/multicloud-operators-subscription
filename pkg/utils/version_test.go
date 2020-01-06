73c4015c48c46bbd339711b3f5008c9918cf65c8
ianzhang366 <55893269+ianzhang366@users.noreply.github.com>
2020-01-06 10:10:24 -0500

    Versioncontrol (#77)
    * fixed empty version field issue
    
    * fix the checking logic
    
    * adding debug trace
    
    * fix lint

--- pkg/utils/version_test.go
+++ pkg/utils/version_test.go
@@ -21,6 +21,8 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/google/go-cmp/cmp"
+
 	dplv1alpha1 "github.com/IBM/multicloud-operators-deployable/pkg/apis/app/v1alpha1"
 )
 
@@ -80,25 +82,20 @@ func generateFakeDpl(generateName, name, version string) dplv1alpha1.Deployable 
 	}
 }
 
-func TestGenerateVersionSet(t *testing.T) {
+func TestGenerateVersionSetWithVersionInfo(t *testing.T) {
 	groupA := "A"
+	groupB := ""
+
 	dpl1 := generateFakeDpl(groupA, "dpl1", "1.0.0")
 	dpl2 := generateFakeDpl(groupA, "dpl2", "2.0.0")
 	dpl3 := generateFakeDpl(groupA, "dpl3", "1.8.0")
-	dpl4 := generateFakeDpl(groupA, "dpl4", "")
-	dpl5 := generateFakeDpl(groupA, "dpl5", "")
 
-	groupB := ""
 	dpl1b := generateFakeDpl(groupB, "dpl1b", "1.0.0")
 	dpl2b := generateFakeDpl(groupB, "dpl2b", "2.0.0")
 	dpl3b := generateFakeDpl(groupB, "dpl3b", "1.6.0")
 
 	test1 := []dplv1alpha1.Deployable{dpl1, dpl2, dpl3}
 	test2 := []dplv1alpha1.Deployable{dpl1, dpl2, dpl3}
-	test3 := []dplv1alpha1.Deployable{dpl4, dpl5}
-
-	test4 := []dplv1alpha1.Deployable{dpl4, dpl5}
-	test5 := []*dplv1alpha1.Deployable{&dpl4, &dpl5}
 
 	test6 := []dplv1alpha1.Deployable{dpl1b, dpl2b, dpl3b}
 
@@ -114,27 +111,6 @@ func TestGenerateVersionSet(t *testing.T) {
 			vsub:     "",
 			dpllist:  DplArrayToDplPointers(test2),
 			result:   "/dpl2",
-		},
-
-		{
-			caseName: "3 subscription has a verion but deployables version is empty",
-			vsub:     "<2.1.0",
-			dpllist:  DplArrayToDplPointers(test3),
-			result:   "/dpl4",
-		},
-
-		{
-			caseName: "4 both subscription and deployable has empty version info",
-			vsub:     "",
-			dpllist:  DplArrayToDplPointers(test4),
-			result:   "/dpl4",
-		},
-
-		{
-			caseName: "5 subscription,deployable has empty version info, passing in deployable pointer",
-			vsub:     "",
-			dpllist:  test5,
-			result:   "/dpl4",
 		},
 	}
 
@@ -165,5 +141,64 @@ func TestGenerateVersionSet(t *testing.T) {
 				t.Errorf("wanted %#v, got %#v", c.result, got)
 			}
 		})
+	}
+}
+
+//While version info is empty, we will treat them as separate resouces, in which
+// we will use the name instead of the generate name for the key of the version set
+func TestGenerateVersionSetWithEmptyVersionInfo(t *testing.T) {
+	groupA := "A"
+	groupB := ""
+
+	dpl2 := generateFakeDpl(groupA, "dpl2", "2.0.0")
+	dpl4 := generateFakeDpl(groupA, "dpl4", "")
+	dpl5 := generateFakeDpl(groupA, "dpl5", "")
+
+	dpl2b := generateFakeDpl(groupB, "dpl2b", "2.0.0")
+	dpl4b := generateFakeDpl(groupB, "dpl4b", "")
+	dpl5b := generateFakeDpl(groupB, "dpl5b", "")
+
+	testCases := []struct {
+		desc       string
+		dpls       []*dplv1alpha1.Deployable
+		vstring    string
+		versionSet map[string]VersionRep
+	}{
+		{
+			desc:    "empty version fields",
+			dpls:    []*dplv1alpha1.Deployable{&dpl4, &dpl5, &dpl4b, &dpl5b},
+			vstring: "",
+			versionSet: map[string]VersionRep{
+				"dpl4":  {DplKey: "/dpl4", Vrange: ">0.0.0"},
+				"dpl5":  {DplKey: "/dpl5", Vrange: ">0.0.0"},
+				"dpl4b": {DplKey: "/dpl4b", Vrange: ">0.0.0"},
+				"dpl5b": {DplKey: "/dpl5b", Vrange: ">0.0.0"},
+			},
+		},
+		{
+			desc:    "empty and valid version fields",
+			dpls:    []*dplv1alpha1.Deployable{&dpl4, &dpl4b, &dpl2, &dpl2b},
+			vstring: "",
+			versionSet: map[string]VersionRep{
+				"dpl4":  {DplKey: "/dpl4", Vrange: ">0.0.0"},
+				"dpl4b": {DplKey: "/dpl4b", Vrange: ">0.0.0"},
+				groupA:  {DplKey: "/dpl2", Vrange: ">2.0.0"},
+				"dpl2b": {DplKey: "/dpl2b", Vrange: ">2.0.0"},
+			},
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			got := GenerateVersionSet(tC.dpls, tC.vstring)
+			assertVersionSet(t, got, tC.versionSet)
+		})
+	}
+}
+
+func assertVersionSet(t *testing.T, got, want map[string]VersionRep) {
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("GenerateVersionSet got error: got %v, want %v", got, want)
+		t.Errorf("MakeGatewayInfo() mismatch (-want +got):\n%s", diff)
 	}
 }
