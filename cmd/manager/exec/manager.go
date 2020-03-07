5b0cc0eeb6a477629f572c80a0a7387a7d93b248
Roke Jung <roke@redhat.com>
2020-03-07 10:46:46 -0500

    Change trvis YAML file extension (#116)
    * Change trvis YAML file extension
    
    Signed-off-by: Roke Jung <rjung@redhat.com>
    
    * Update .golangci.yml
    
    * Update manager.go
    
    * Update .travis.yml
    
    Co-authored-by: Roke Jung <rjung@redhat.com>

--- cmd/manager/exec/manager.go
+++ cmd/manager/exec/manager.go
@@ -24,7 +24,6 @@ import (
 	kubemetrics "github.com/operator-framework/operator-sdk/pkg/kube-metrics"
 	"github.com/operator-framework/operator-sdk/pkg/leader"
 	"github.com/operator-framework/operator-sdk/pkg/metrics"
-	"github.com/operator-framework/operator-sdk/pkg/restmapper"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
 	"github.com/prometheus/common/log"
 	v1 "k8s.io/api/core/v1"
@@ -77,7 +76,6 @@ func RunManager(sig <-chan struct{}) {
 	// Create a new Cmd to provide shared dependencies and start components
 	mgr, err := manager.New(cfg, manager.Options{
 		Namespace:          namespace,
-		MapperProvider:     restmapper.NewDynamicRESTMapper,
 		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
 	})
 	if err != nil {
