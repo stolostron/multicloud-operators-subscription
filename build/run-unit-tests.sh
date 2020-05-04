adb320aa1eb1f8fba8c89539f26a3268bcb6f64d
ianzhang366 <ian.w.zhang@redhat.com>
2020-05-04 11:10:17 -0400

    upgrade kubebuilder version on travis
    Signed-off-by: ianzhang366 <ian.w.zhang@redhat.com>

--- build/run-unit-tests.sh
+++ build/run-unit-tests.sh
@@ -21,13 +21,13 @@ echo "Install Kubebuilder components for test framework usage!"
 
 _OS=$(go env GOOS)
 _ARCH=$(go env GOARCH)
-
+KubeBuilderVersion="2.3.1"
 # download kubebuilder and extract it to tmp
-curl -L https://go.kubebuilder.io/dl/2.2.0/"${_OS}"/"${_ARCH}" | tar -xz -C /tmp/
+curl -L https://go.kubebuilder.io/dl/"$KubeBuilderVersion"/"${_OS}"/"${_ARCH}" | tar -xz -C /tmp/
 
 # move to a long-term location and put it on your path
 # (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
-sudo mv /tmp/kubebuilder_2.2.0_"${_OS}"_"${_ARCH}" /usr/local/kubebuilder
+sudo mv /tmp/kubebuilder_"$KubeBuilderVersion"_"${_OS}"_"${_ARCH}" /usr/local/kubebuilder
 export PATH=$PATH:/usr/local/kubebuilder/bin
 
 # Run unit test
