a548219b7c997ecf35db706fbc4674ef2f911c95
mikeshng <58747157+mikeshng@users.noreply.github.com>
2020-06-29 17:03:29 -0400

    ensure helmrelease URLs are valid (#227)
    Signed-off-by: Mike Ng <ming@redhat.com>

--- pkg/utils/helmrepo_test.go
+++ pkg/utils/helmrepo_test.go
@@ -23,6 +23,9 @@ import (
 	"github.com/onsi/gomega"
 	clientsetx "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/helm/pkg/repo"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
@@ -30,6 +33,34 @@ import (
 	releasev1 "github.com/open-cluster-management/multicloud-operators-subscription-release/pkg/apis/apps/v1"
 	appv1 "github.com/open-cluster-management/multicloud-operators-subscription/pkg/apis/apps/v1"
 	appv1alpha1 "github.com/open-cluster-management/multicloud-operators-subscription/pkg/apis/apps/v1"
+)
+
+var (
+	helmkey = types.NamespacedName{
+		Name:      "testhelmkey",
+		Namespace: "default",
+	}
+
+	helmchn = &chnv1.Channel{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      helmkey.Name,
+			Namespace: helmkey.Namespace,
+		},
+		Spec: chnv1.ChannelSpec{
+			Type:     "HelmRepo",
+			Pathname: "https://github.com/open-cluster-management/multicloud-operators-subscription/test/helm",
+		},
+	}
+
+	helmsub = &appv1.Subscription{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      helmkey.Name,
+			Namespace: helmkey.Namespace,
+		},
+		Spec: appv1.SubscriptionSpec{
+			Channel: helmkey.String(),
+		},
+	}
 )
 
 func TestGetPackageAlias(t *testing.T) {
@@ -108,6 +139,32 @@ func TestCreateOrUpdateHelmChart(t *testing.T) {
 	helmrelease, err = CreateOrUpdateHelmChart("chart1", "chart1-1.0.0", indexFile.Entries["chart1"], c, githubchn, githubsub)
 	g.Expect(err).NotTo(gomega.HaveOccurred())
 	g.Expect(helmrelease).NotTo(gomega.BeNil())
+
+	var relativeUrls []string
+	relativeUrls = append(relativeUrls, "my-app-0.1.0.tgz")
+
+	var relativeChartVersions []*repo.ChartVersion
+	relativeChartVersions = append(relativeChartVersions, &repo.ChartVersion{URLs: relativeUrls})
+
+	helmrelease, err = CreateOrUpdateHelmChart("my-app", "my-app-0.1.0", relativeChartVersions, c, helmchn, helmsub)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	g.Expect(helmrelease).NotTo(gomega.BeNil())
+	g.Expect(helmrelease.Repo.Source.HelmRepo.Urls[0]).
+		Should(gomega.Equal(
+			"https://github.com/open-cluster-management/multicloud-operators-subscription/test/helm/my-app-0.1.0.tgz"))
+
+	var fullUrls []string
+	fullUrls = append(fullUrls, "https://kubernetes-charts.storage.googleapis.com/nginx-ingress-1.36.3.tgz")
+
+	var fullChartVersions []*repo.ChartVersion
+	fullChartVersions = append(fullChartVersions, &repo.ChartVersion{URLs: fullUrls})
+
+	helmrelease, err = CreateOrUpdateHelmChart("nginx-ingress", "nginx-ingress-1.36.3", fullChartVersions, c, helmchn, helmsub)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	g.Expect(helmrelease).NotTo(gomega.BeNil())
+	g.Expect(helmrelease.Repo.Source.HelmRepo.Urls[0]).
+		Should(gomega.Equal(
+			"https://kubernetes-charts.storage.googleapis.com/nginx-ingress-1.36.3.tgz"))
 }
 
 func TestCheckTillerVersion(t *testing.T) {
@@ -356,4 +413,11 @@ func TestDeleteHelmReleaseCRD(t *testing.T) {
 	hrlist = &releasev1.HelmReleaseList{}
 	err = runtimeClient.List(context.TODO(), hrlist, &client.ListOptions{})
 	g.Expect(!errors.IsNotFound(err)).To(gomega.BeTrue())
+}
+
+func TestIsURL(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	g.Expect(IsURL("https://kubernetes-charts.storage.googleapis.com/nginx-ingress-1.40.1.tgz")).To(gomega.BeTrue())
+	g.Expect(IsURL("nginx-ingress-1.40.1.tgz")).To(gomega.BeFalse())
 }
