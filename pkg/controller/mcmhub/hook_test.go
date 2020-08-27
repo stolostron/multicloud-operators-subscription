235ff3f20a068e9fecb7328ea34b9f9e0c95e061
Ian Zhang <izhang@redhat.com>
2020-08-27 12:08:40 -0400

    adding test framework
    Signed-off-by: Ian Zhang <izhang@redhat.com>

--- pkg/controller/mcmhub/hook_test.go
+++ pkg/controller/mcmhub/hook_test.go
@@ -21,7 +21,9 @@ import (
 
 	"github.com/onsi/gomega"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
+	ansiblejob "github.com/open-cluster-management/ansiblejob-go-lib/api/v1alpha1"
 	chnv1 "github.com/open-cluster-management/multicloud-operators-channel/pkg/apis/apps/v1"
 	subv1 "github.com/open-cluster-management/multicloud-operators-subscription/pkg/apis/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -34,7 +36,7 @@ func TestPreHookLogic(t *testing.T) {
 	mgr, err := manager.New(cfg, manager.Options{MetricsBindAddress: "0"})
 	g.Expect(err).NotTo(gomega.HaveOccurred())
 
-	c = mgr.GetClient()
+	k8sClt := mgr.GetClient()
 
 	stopMgr, mgrStopped := StartTestManager(mgr, g)
 
@@ -45,17 +47,14 @@ func TestPreHookLogic(t *testing.T) {
 
 	g.Expect(mgr.GetCache().WaitForCacheSync(stopMgr)).Should(gomega.BeTrue())
 
-	g.Expect(c.Create(context.TODO(), cfgMap)).Should(gomega.Succeed())
+	hookRequeueInterval := time.Second * 1
+	rec := newReconciler(mgr, hookRequeueInterval).(*ReconcileSubscription)
 
-	defer func() {
-		g.Expect(c.Delete(context.TODO(), cfgMap)).Should(gomega.Succeed())
-	}()
-
-	rec := newReconciler(mgr, time.Second*1).(*ReconcileSubscription)
-
-	subKey := types.NamespacedName{Name: "t-sub", Namespace: "t-ns"}
-	chnKey := types.NamespacedName{Name: "t-chn", Namespace: "t-ns"}
-	ansibleGitURl := "https://"
+	testNs := "ansible"
+	subKey := types.NamespacedName{Name: "t-sub", Namespace: testNs}
+	chnKey := types.NamespacedName{Name: "t-chn", Namespace: testNs}
+	preAnsibleKey := types.NamespacedName{Name: "prehook-test", Namespace: testNs}
+	ansibleGitURl := "https://github.com/ianzhang366/acm-applifecycle-samples"
 
 	chn := &chnv1.Channel{
 		ObjectMeta: metav1.ObjectMeta{
@@ -68,18 +67,48 @@ func TestPreHookLogic(t *testing.T) {
 		},
 	}
 
-	//subscription doen't have prehook, then reconcile should process
 	subInsWithoutPrehook := &subv1.Subscription{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      subKey.Name,
 			Namespace: subKey.Namespace,
+			Annotations: map[string]string{
+				subv1.AnnotationGitBranch: "master",
+				subv1.AnnotationGitPath:   "git-ops/ansible/resources",
+			},
 		},
 		Spec: subv1.SubscriptionSpec{
 			Channel: chnKey.String(),
+			Prehook: preAnsibleKey.String(),
 		},
 	}
 
+	ctx := context.TODO()
+	g.Expect(k8sClt.Create(ctx, chn.DeepCopy())).Should(gomega.Succeed())
+
+	defer func() {
+		g.Expect(k8sClt.Delete(ctx, chn.DeepCopy())).Should(gomega.Succeed())
+	}()
+
+	//asumming the github have the ansible YAML
 	//subscription with prehook, then after reconcile, we should be able to
 	//detect the ansibleJob instance from cluster and the subscription status
 	//shouldn't be propagated
+	g.Expect(k8sClt.Create(ctx, subInsWithoutPrehook.DeepCopy())).Should(gomega.Succeed())
+
+	defer func() {
+		g.Expect(k8sClt.Delete(ctx, subInsWithoutPrehook.DeepCopy())).Should(gomega.Succeed())
+	}()
+
+	r, err := rec.Reconcile(reconcile.Request{NamespacedName: subKey})
+
+	g.Expect(err).Should(gomega.Succeed())
+	g.Expect(r.RequeueAfter).Should(gomega.Equal(hookRequeueInterval))
+
+	ansibleIns := &ansiblejob.AnsibleJob{}
+
+	g.Eventually(k8sClt.Get(ctx, preAnsibleKey, ansibleIns), time.Second*5).Should(gomega.Succeed())
+
+	defer func() {
+		g.Expect(k8sClt.Delete(ctx, ansibleIns)).Should(gomega.Succeed())
+	}()
 }
