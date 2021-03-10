bbaa28d47ee8f66b1baaac05668345923d8df383
Mike Ng <ming@redhat.com>
2021-03-10 11:51:50 -0500

    add setup tests for prow (#433)
    Signed-off-by: Mike Ng <ming@redhat.com>

---
+++ build/setup-tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+###############################################################################
+# (c) Copyright IBM Corporation 2019, 2020. All Rights Reserved.
+# Note to U.S. Government Users Restricted Rights:
+# U.S. Government Users Restricted Rights - Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+# Copyright (c) Red Hat, Inc.
+# Copyright Contributors to the Open Cluster Management project
+###############################################################################
+
+os=$(go env GOOS)
+arch=$(go env GOARCH)
+
+# download kubebuilder and extract it to tmp
+curl -L https://go.kubebuilder.io/dl/2.3.1/${os}/${arch} | tar -xz -C /tmp/
+
+rm -rf /usr/local/kubebuilder
+mv /tmp/kubebuilder_2.3.1_${os}_${arch} /usr/local/kubebuilder
