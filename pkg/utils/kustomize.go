dca167be3808c7bb29d1f53f493ad0bafd6292fd
Mike Ng <ming@redhat.com>
2021-03-25 11:26:59 -0400

    change community-latest reference to latest, update kustomize package override check, rename a test
    Signed-off-by: Mike Ng <ming@redhat.com>

--- pkg/utils/kustomize.go
+++ pkg/utils/kustomize.go
@@ -55,7 +55,7 @@ func RunKustomizeBuild(kustomizeDir string) ([]byte, error) {
 }
 
 func CheckPackageOverride(ov *appv1.Overrides) error {
-	if ov.PackageOverrides == nil {
+	if ov.PackageOverrides == nil || len(ov.PackageOverrides) < 1 {
 		return errors.New("no PackageOverride is specified. Skipping to override kustomization")
 	}
 
