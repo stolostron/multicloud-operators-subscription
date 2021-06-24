38c456c26d2ab6a041201703ec17a0ee80635f1e
Philip Wu <phwu@redhat.com>
2021-06-24 01:01:27 -0400

    Fix start time border case
    Signed-off-by: Philip Wu <phwu@redhat.com>

--- pkg/utils/timewindow.go
+++ pkg/utils/timewindow.go
@@ -324,7 +324,7 @@ func tillNextSlotInCurrentDay(slots []hourRangesInTime, cur time.Time) time.Dura
 		if gt.Sub(slot.start) > 0 && (gt.Sub(slot.end) <= 0 || isMidnight(slot.end)) {
 			// not blocked now
 			return time.Duration(0)
-		} else if gt.Sub(slot.start) < 0 {
+		} else if gt.Sub(slot.start) <= 0 {
 			// there is available time slot later in current day
 			return slot.start.Sub(gt)
 		}
