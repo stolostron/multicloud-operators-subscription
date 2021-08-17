d9087221b7150e7ef7dae21513223c4af043eba6
Xiangjing Li <xiangli@redhat.com>
2021-08-17 18:41:23 -0400

    add new unit test - delete deployed deployment cr
    Signed-off-by: Xiangjing Li <xiangli@redhat.com>

--- pkg/synchronizer/kubernetes/synchronizer_test.go
+++ pkg/synchronizer/kubernetes/synchronizer_test.go
@@ -20,6 +20,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	appSubStatusV1alpha1 "github.com/open-cluster-management/multicloud-operators-subscription/pkg/apis/apps/v1alpha1"
+	apps "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -36,7 +37,7 @@ var (
 		Namespace: "appsub-ns-1",
 	}
 
-	workloadConfigmap1 = corev1.ConfigMap{
+	workload1Configmap = corev1.ConfigMap{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "ConfigMap",
 			APIVersion: "v1",
@@ -46,14 +47,44 @@ var (
 			Namespace: "default",
 		},
 	}
+
+	selector = map[string]string{"a": "b"}
+
+	workload2Deployment = apps.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "deployment1",
+			Namespace: "default",
+		},
+		Spec: apps.DeploymentSpec{
+			Replicas: func() *int32 { i := int32(1); return &i }(),
+			Selector: &metav1.LabelSelector{MatchLabels: selector},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: selector,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "foobar",
+							Image: "foo/bar",
+						},
+					},
+				},
+			},
+		},
+	}
 )
 
 var _ = Describe("test Delete Single Subscribed Resource", func() {
-	It("should delete the confimap without failure", func() {
+	It("should delete the confimap and deployment without failure", func() {
 		sync, err := CreateSynchronizer(k8sManager.GetConfig(), k8sManager.GetConfig(), k8sManager.GetScheme(), &host, 2, nil)
 		Expect(err).NotTo(HaveOccurred())
 
-		workload1 := workloadConfigmap1.DeepCopy()
+		workload1 := workload1Configmap.DeepCopy()
 		Expect(k8sClient.Create(context.TODO(), workload1)).NotTo(HaveOccurred())
 
 		defer k8sClient.Delete(context.TODO(), workload1)
@@ -63,6 +94,21 @@ var _ = Describe("test Delete Single Subscribed Resource", func() {
 			Namespace:  "default",
 			ApiVersion: "v1",
 			Kind:       "ConfigMap",
+		}
+
+		err = sync.DeleteSingleSubscribedResource(hostSub, pkgStatus)
+		Expect(err).NotTo(HaveOccurred())
+
+		workload2 := workload2Deployment.DeepCopy()
+		Expect(k8sClient.Create(context.TODO(), workload2)).NotTo(HaveOccurred())
+
+		defer k8sClient.Delete(context.TODO(), workload2)
+
+		pkgStatus = appSubStatusV1alpha1.SubscriptionUnitStatus{
+			Name:       "deployment1",
+			Namespace:  "default",
+			ApiVersion: "apps/v1",
+			Kind:       "Deployment",
 		}
 
 		err = sync.DeleteSingleSubscribedResource(hostSub, pkgStatus)
