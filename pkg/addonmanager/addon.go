4165c780047002527943e67e93527907a0d9f5fe
Mike Ng <ming@redhat.com>
2021-10-05 14:35:45 -0400

    fix addon deployment
    Signed-off-by: Mike Ng <ming@redhat.com>

--- pkg/addonmanager/addon.go
+++ pkg/addonmanager/addon.go
@@ -32,7 +32,7 @@ var (
 		"deploy/managed-common/apps.open-cluster-management.io_helmreleases_crd.yaml",
 		"deploy/managed-common/apps.open-cluster-management.io_subscriptions_crd_v1.yaml",
 		"deploy/managed-common/apps.open-cluster-management.io_placementrules_crd.yaml",
-		"deploy/managed-common/apps.open-cluster-management.io_subscriptionstatuses_crd_v1alpha1",
+		"deploy/managed-common/apps.open-cluster-management.io_subscriptionstatuses_crd_v1alpha1.yaml",
 		"deploy/managed-common/clusterrole_binding.yaml",
 		"deploy/managed-common/clusterrole.yaml",
 		"deploy/managed-common/service_account.yaml",
