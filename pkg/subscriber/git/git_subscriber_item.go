9f6951d84df56ebb952126793477e8ed981f04a2
Roke Jung <roke@redhat.com>
2021-11-16 15:14:31 -0500

    fix skip ssl (#41)
    Signed-off-by: Roke Jung <roke@redhat.com>

--- pkg/subscriber/git/git_subscriber_item.go
+++ pkg/subscriber/git/git_subscriber_item.go
@@ -751,6 +751,7 @@ func (ghsi *SubscriberItem) cloneGitRepo() (commitID string, err error) {
 	}
 
 	primaryChannelConnectionConfig.RepoURL = ghsi.Channel.Spec.Pathname
+	primaryChannelConnectionConfig.InsecureSkipVerify = ghsi.Channel.Spec.InsecureSkipVerify
 	cloneOptions.PrimaryConnectionOption = primaryChannelConnectionConfig
 
 	// Get the secondary channel connection options
@@ -763,6 +764,7 @@ func (ghsi *SubscriberItem) cloneGitRepo() (commitID string, err error) {
 		}
 
 		secondaryChannelConnectionConfig.RepoURL = ghsi.SecondaryChannel.Spec.Pathname
+		secondaryChannelConnectionConfig.InsecureSkipVerify = ghsi.SecondaryChannel.Spec.InsecureSkipVerify
 		cloneOptions.SecondaryConnectionOption = secondaryChannelConnectionConfig
 	}
 
