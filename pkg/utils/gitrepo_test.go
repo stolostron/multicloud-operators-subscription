74fcca5f2f2c7cc0f76d06c878187e3e2bd3fc54
Jonathan Marcantonio <jmarcant@redhat.com>
2022-06-03 13:02:11 -0400

    Add test case to verify certificate chain
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- pkg/utils/gitrepo_test.go
+++ pkg/utils/gitrepo_test.go
@@ -16,10 +16,13 @@ package utils
 
 import (
 	"context"
+	"crypto/tls"
+	"encoding/pem"
 	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 	"time"
 
@@ -193,6 +196,60 @@ data:
 
 	if len(ret) != 1 {
 		t.Errorf("faild to parse yaml objects, wanted %v, got %v", 1, len(ret))
+	}
+}
+
+func TestGetCertChain(t *testing.T) {
+	validcert := `
+-----BEGIN CERTIFICATE-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAlRuRnThUjU8/prwYxbty
+WPT9pURI3lbsKMiB6Fn/VHOKE13p4D8xgOCADpdRagdT6n4etr9atzDKUSvpMtR3
+CP5noNc97WiNCggBjVWhs7szEe8ugyqF23XwpHQ6uV1LKH50m92MbOWfCtjU9p/x
+qhNpQQ1AZhqNy5Gevap5k8XzRmjSldNAFZMY7Yv3Gi+nyCwGwpVtBUwhuLzgNFK/
+yDtw2WcWmUU7NuC8Q6MWvPebxVtCfVp/iQU6q60yyt6aGOBkhAX0LpKAEhKidixY
+nP9PNVBvxgu3XZ4P36gZV6+ummKdBVnc3NqwBLu5+CcdRdusmHPHd5pHf4/38Z3/
+6qU2a/fPvWzceVTEgZ47QjFMTCTmCwNt29cvi7zZeQzjtwQgn4ipN9NibRH/Ax/q
+TbIzHfrJ1xa2RteWSdFjwtxi9C20HUkjXSeI4YlzQMH0fPX6KCE7aVePTOnB69I/
+a9/q96DiXZajwlpq3wFctrs1oXqBp5DVrCIj8hU2wNgB7LtQ1mCtsYz//heai0K9
+PhE4X6hiE0YmeAZjR0uHl8M/5aW9xCoJ72+12kKpWAa0SFRWLy6FejNYCYpkupVJ
+yecLk/4L1W0l6jQQZnWErXZYe0PNFcmwGXy1Rep83kfBRNKRy5tvocalLlwXLdUk
+AIU+2GKjyT3iMuzZxxFxPFMCAwEAAQ==
+-----END CERTIFICATE-----
+and some more`
+
+	bytearray, _ := pem.Decode([]byte(validcert))
+
+	testCases := []struct {
+		desc   string
+		certs  string
+		wanted tls.Certificate
+	}{
+		{
+			desc:   "invalid cert",
+			certs:  "",
+			wanted: tls.Certificate{},
+		},
+		{
+			desc: "empty cert",
+			certs: `
+-----BEGIN CERTIFICATE-----
+-----END CERTIFICATE-----
+			`,
+			wanted: tls.Certificate{Certificate: [][]byte{{}}},
+		},
+		{
+			desc:   "valid cert",
+			certs:  validcert,
+			wanted: tls.Certificate{Certificate: [][]byte{bytearray.Bytes}},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			got := getCertChain(tC.certs)
+			if !reflect.DeepEqual(got, tC.wanted) {
+				t.Errorf("wanted %v, got %v", tC.wanted, got)
+			}
+		})
 	}
 }
 
