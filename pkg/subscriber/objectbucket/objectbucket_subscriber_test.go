4a2fe223cfa717dfa5290a2a51a522c4b58335ff
Xiangjing Li <55890329+xiangjingli@users.noreply.github.com>
2022-06-28 17:41:18 -0400

    add unit test for object store subscriber (#773)
    Signed-off-by: Xiangjing Li <xiangli@redhat.com>

--- pkg/subscriber/objectbucket/objectbucket_subscriber_test.go
+++ pkg/subscriber/objectbucket/objectbucket_subscriber_test.go
@@ -16,17 +16,22 @@ package objectbucket
 
 import (
 	"context"
+	"encoding/json"
 	"testing"
 	"time"
 
+	"github.com/ghodss/yaml"
 	"github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	chnv1alpha1 "open-cluster-management.io/multicloud-operators-channel/pkg/apis/apps/v1"
 	appv1alpha1 "open-cluster-management.io/multicloud-operators-subscription/pkg/apis/apps/v1"
+	kubesynchronizer "open-cluster-management.io/multicloud-operators-subscription/pkg/synchronizer/kubernetes"
 )
 
 var c client.Client
@@ -41,27 +46,92 @@ var (
 		Name:      "test",
 		Namespace: "default",
 	}
-	helmchn = &chnv1alpha1.Channel{
+
+	channelSecret = &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "obj-secret",
+			Namespace: sharedkey.Namespace,
+		},
+		Data: map[string][]byte{
+			"AccessKeyID":     []byte("test-access-id"),
+			"SecretAccessKey": []byte("test-secret-access-key"),
+			"Region":          []byte("test-region"),
+		},
+	}
+
+	channelConfigMap = &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "obj-configmap",
+			Namespace: sharedkey.Namespace,
+		},
+		Data: map[string]string{
+			"name1": "value1",
+			"name2": "value2",
+		},
+	}
+
+	objConfigMap = &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				"anno1": "anno1",
+			},
+			Labels: map[string]string{
+				"label1": "label",
+			},
+			Name:      "obj-configmap-2",
+			Namespace: "obj-ns-2",
+		},
+		Data: map[string]string{
+			"name3": "value3",
+		},
+	}
+
+	objChannel = &chnv1alpha1.Channel{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      sharedkey.Name,
 			Namespace: sharedkey.Namespace,
 		},
 		Spec: chnv1alpha1.ChannelSpec{
 			Pathname: "http://minio-minio.apps.hivemind-b.aws.red-chesterfield.com/fake-bucket",
+			SecretRef: &corev1.ObjectReference{
+				Name: "obj-secret",
+			},
+			ConfigMapRef: &corev1.ObjectReference{
+				Name: "obj-configmap",
+			},
 		},
 	}
-	helmsub = &appv1alpha1.Subscription{
+
+	objSub = &appv1alpha1.Subscription{
 		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				"apps.open-cluster-management.io/reconcile-option": "merge",
+				"apps.open-cluster-management.io/":                 "cluster-admin",
+			},
+			Labels: map[string]string{
+				"app.kubernetes.io/part-of": "appsub-obj-1",
+			},
 			Name:      sharedkey.Name,
 			Namespace: sharedkey.Namespace,
 		},
 		Spec: appv1alpha1.SubscriptionSpec{
 			Channel: sharedkey.String(),
+			Package: objConfigMap.Name,
+			PackageFilter: &appv1alpha1.PackageFilter{
+				LabelSelector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"label1": "label",
+					},
+				},
+				Annotations: map[string]string{
+					"anno1": "anno1",
+				},
+			},
 		},
-	}
-	subitem = &appv1alpha1.SubscriberItem{
-		Subscription: helmsub,
-		Channel:      helmchn,
 	}
 )
 
@@ -85,8 +155,60 @@ func TestObjectSubscriber(t *testing.T) {
 		mgrStopped.Wait()
 	}()
 
-	// connect to a fake object store, should expect connection failure now.
-	err = defaultSubscriber.SubscribeItem(subitem)
-
+	defaultExtension := &kubesynchronizer.SubscriptionExtension{}
+	syncid := &types.NamespacedName{
+		Namespace: "cluster1",
+		Name:      "cluster1",
+	}
+	defaultSynchronizer, err := kubesynchronizer.CreateSynchronizer(mgr.GetConfig(), cfg, mgr.GetScheme(), syncid, 60, defaultExtension, true, false)
 	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+	obssubitem := &SubscriberItem{}
+	obssubitem.synchronizer = defaultSynchronizer
+	obssubitem.Channel = objChannel
+	obssubitem.SecondaryChannel = objChannel
+	obssubitem.Subscription = objSub
+	obssubitem.ChannelSecret = channelSecret
+	obssubitem.ChannelConfigMap = channelConfigMap
+
+	// Test 1: test get object Channel Config
+	endpoint, accessKeyID, secretAccessKey, region, err := obssubitem.getChannelConfig(true)
+
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	g.Expect(endpoint).To(gomega.Equal("http://minio-minio.apps.hivemind-b.aws.red-chesterfield.com"))
+	g.Expect(accessKeyID).To(gomega.Equal("test-access-id"))
+	g.Expect(secretAccessKey).To(gomega.Equal("test-secret-access-key"))
+	g.Expect(region).To(gomega.Equal("test-region"))
+
+	obssubitem.doSubscription()
+
+	// Test 2: test doSubscribeManifest
+	objByte, err := json.Marshal(objConfigMap)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+
+	objTemplate := &unstructured.Unstructured{}
+	err = yaml.Unmarshal(objByte, objTemplate)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+
+	// If cluster admin, the original object namespace is respected
+	obssubitem.clusterAdmin = true
+	resource, err := obssubitem.doSubscribeManifest(objTemplate)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+
+	objNamespace := objConfigMap.GetNamespace()
+
+	g.Expect(resource.Resource.GetNamespace()).To(gomega.Equal(objNamespace))
+
+	// If not cluster admin, the original object namespace is replaced into the appsub namespace
+	obssubitem.clusterAdmin = false
+	resource, err = obssubitem.doSubscribeManifest(objTemplate)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+
+	appsubNamespace := objSub.GetNamespace()
+
+	g.Expect(resource.Resource.GetNamespace()).To(gomega.Equal(appsubNamespace))
+
+	// check if the app lable is appened to the resource template
+	appLabel := resource.Resource.GetLabels()["app.kubernetes.io/part-of"]
+	g.Expect(appLabel).To(gomega.Equal("appsub-obj-1"))
 }
