cc706fc34e774807e9eddae03bb0d60cc616eaa1
Maggie Chen <magchen@redhat.com>
2022-07-05 14:45:31 -0400

    Merge pull request #210 from open-cluster-management-io/add-unit-test (#777)
    * Merge pull request #210 from open-cluster-management-io/add-unit-test
    
    Add unit test for do-not-delete
    
    Signed-off-by: Maggie Chen <magchen@redhat.com>
    
    * change cluster name
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    Co-authored-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- pkg/synchronizer/kubernetes/synchronizer_test.go
+++ pkg/synchronizer/kubernetes/synchronizer_test.go
@@ -33,8 +33,8 @@ import (
 
 var (
 	host = types.NamespacedName{
-		Name:      "cluster1",
-		Namespace: "cluster1",
+		Name:      "cluster2",
+		Namespace: "cluster2",
 	}
 	hostworkload4 = types.NamespacedName{
 		Name:      "subscription-4",
@@ -104,6 +104,19 @@ var (
 			Namespace: "default",
 			Annotations: map[string]string{
 				appv1alpha1.AnnotationHosting: "not owned",
+			},
+		},
+	}
+	workload4Configmap = corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "configmap2",
+			Namespace: "default",
+			Annotations: map[string]string{
+				appv1alpha1.AnnotationResourceDoNotDeleteOption: "true",
 			},
 		},
 	}
@@ -204,9 +217,6 @@ var _ = Describe("test Delete Single Subscribed Resource", func() {
 	})
 
 	It("should not be owned by the subscription", func() {
-		sync, err := CreateSynchronizer(k8sManager.GetConfig(), k8sManager.GetConfig(), k8sManager.GetScheme(), &host, 2, nil, false, false)
-		Expect(err).NotTo(HaveOccurred())
-
 		workload1 := workload3Configmap.DeepCopy()
 		Expect(k8sClient.Create(context.TODO(), workload1)).NotTo(HaveOccurred())
 
@@ -221,6 +231,23 @@ var _ = Describe("test Delete Single Subscribed Resource", func() {
 
 		err = sync.DeleteSingleSubscribedResource(hostSub, pkgStatus)
 		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should not delete if the resource has do-not-delete annotation", func() {
+		workload1 := workload4Configmap.DeepCopy()
+		Expect(k8sClient.Create(context.TODO(), workload1)).NotTo(HaveOccurred())
+
+		defer k8sClient.Delete(context.TODO(), workload1)
+
+		pkgStatus := appSubStatusV1alpha1.SubscriptionUnitStatus{
+			Name:       "configmap2",
+			Namespace:  "default",
+			APIVersion: "v1",
+			Kind:       "ConfigMap",
+		}
+
+		err = sync.DeleteSingleSubscribedResource(hostSub, pkgStatus)
+		Expect(err).To(BeNil())
 	})
 })
 
