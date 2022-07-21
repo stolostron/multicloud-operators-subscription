7641e98dfbb94c8d6febacf2b7167f667bd3dafd
Jonathan Marcantonio <jmarcant@redhat.com>
2022-07-21 11:00:28 -0400

    Add test for DeleteReferredObjects
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- pkg/utils/reference_test.go
+++ pkg/utils/reference_test.go
@@ -27,6 +27,7 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	appv1alpha1 "open-cluster-management.io/multicloud-operators-subscription/pkg/apis/apps/v1"
@@ -412,6 +413,81 @@ func TestListAndDeployReferredObject(t *testing.T) {
 
 			time.Sleep(time.Second * 2)
 			t.Logf("at test case %v got referred object %v ", tC.desc, gotSrt)
+		})
+	}
+}
+
+func TestDeleteReferredObjects(t *testing.T) {
+	ownerName := "sub-a"
+	ownerUID := types.UID("sub-uid")
+
+	ownera := &appv1alpha1.Subscription{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      ownerName,
+			Namespace: "default",
+			UID:       ownerUID,
+		},
+		Spec: appv1alpha1.SubscriptionSpec{
+			Channel: chKey.String(),
+		},
+	}
+
+	testCases := []struct {
+		desc    string
+		refSrt  *corev1.Secret
+		itemLen int
+	}{
+		{
+			desc: "only one owner, 0 items",
+			refSrt: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					ResourceVersion: srtGVK.String(),
+					Name:            refSrtName,
+					OwnerReferences: []metav1.OwnerReference{
+						{Kind: SubscriptionGVK.Kind, APIVersion: SubscriptionGVK.Version, Name: ownera.GetName(), UID: ownera.GetUID()},
+					},
+				},
+			},
+			itemLen: 0,
+		},
+	}
+
+	g := NewGomegaWithT(t)
+	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
+	// channel when it is finid'Cu
+	mgr, err := manager.New(cfg, manager.Options{MetricsBindAddress: "0"})
+	g.Expect(err).NotTo(HaveOccurred())
+
+	c = mgr.GetClient()
+
+	ctx, cancel := context.WithTimeout(context.TODO(), 5*time.Minute)
+	mgrStopped := StartTestManager(ctx, mgr, g)
+
+	defer func() {
+		cancel()
+		mgrStopped.Wait()
+	}()
+
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			rq := types.NamespacedName{Namespace: ownera.GetNamespace(), Name: ownera.GetName()}
+
+			err = DeleteReferredObjects(c, rq, srtGVK)
+			g.Expect(err).Should(BeNil())
+
+			time.Sleep(time.Second * 2)
+			tmp := &corev1.SecretList{}
+
+			opts := &client.ListOptions{
+				Namespace: ownera.GetNamespace(),
+			}
+			g.Expect(c.List(context.TODO(), tmp, opts)).NotTo(HaveOccurred())
+
+			if len(tC.refSrt.GetOwnerReferences()) > 2 {
+				g.Expect(tmp.Items).Should(HaveLen(tC.itemLen))
+				g.Expect(c.Delete(context.TODO(), tC.refSrt)).NotTo(HaveOccurred())
+			}
+			g.Expect(tmp.Items).Should(HaveLen(tC.itemLen))
 		})
 	}
 }
