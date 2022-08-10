3f20e31145d5ab1b0a6192ddf34a4059b43251c7
Jonathan Marcantonio <jmarcant@redhat.com>
2022-08-10 09:47:11 -0400

    Add test for GetCertChain
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- pkg/helmrelease/utils/helmrepoutils_test.go
+++ pkg/helmrelease/utils/helmrepoutils_test.go
@@ -18,9 +18,12 @@ package utils
 
 import (
 	"context"
+	"crypto/tls"
+	"encoding/pem"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 	"time"
 
@@ -593,6 +596,60 @@ func TestGetKnownHostFromURL(t *testing.T) {
 			got := getKnownHostFromURL(tC.sshURL, tC.filepath)
 			if got != nil && !tC.expectError { // If error and we don't expect an error
 				t.Errorf("wanted error %v, got %v", tC.expectError, got)
+			}
+		})
+	}
+}
+
+func TestGetCertChain(t *testing.T) {
+	validCert := `
+-----BEGIN CERTIFICATE-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAlRuRnThUjU8/prwYxbty
+WPT9pURI3lbsKMiB6Fn/VHOKE13p4D8xgOCADpdRagdT6n4etr9atzDKUSvpMtR3
+CP5noNc97WiNCggBjVWhs7szEe8ugyqF23XwpHQ6uV1LKH50m92MbOWfCtjU9p/x
+qhNpQQ1AZhqNy5Gevap5k8XzRmjSldNAFZMY7Yv3Gi+nyCwGwpVtBUwhuLzgNFK/
+yDtw2WcWmUU7NuC8Q6MWvPebxVtCfVp/iQU6q60yyt6aGOBkhAX0LpKAEhKidixY
+nP9PNVBvxgu3XZ4P36gZV6+ummKdBVnc3NqwBLu5+CcdRdusmHPHd5pHf4/38Z3/
+6qU2a/fPvWzceVTEgZ47QjFMTCTmCwNt29cvi7zZeQzjtwQgn4ipN9NibRH/Ax/q
+TbIzHfrJ1xa2RteWSdFjwtxi9C20HUkjXSeI4YlzQMH0fPX6KCE7aVePTOnB69I/
+a9/q96DiXZajwlpq3wFctrs1oXqBp5DVrCIj8hU2wNgB7LtQ1mCtsYz//heai0K9
+PhE4X6hiE0YmeAZjR0uHl8M/5aW9xCoJ72+12kKpWAa0SFRWLy6FejNYCYpkupVJ
+yecLk/4L1W0l6jQQZnWErXZYe0PNFcmwGXy1Rep83kfBRNKRy5tvocalLlwXLdUk
+AIU+2GKjyT3iMuzZxxFxPFMCAwEAAQ==
+-----END CERTIFICATE-----
+and some more`
+
+	byteArr, _ := pem.Decode([]byte(validCert))
+
+	testCases := []struct {
+		desc   string
+		certs  string
+		wanted tls.Certificate
+	}{
+		{
+			desc:   "invalid cert",
+			certs:  "",
+			wanted: tls.Certificate{},
+		},
+		{
+			desc: "empty cert",
+			certs: `
+-----BEGIN CERTIFICATE-----
+-----END CERTIFICATE-----
+			`,
+			wanted: tls.Certificate{Certificate: [][]byte{{}}},
+		},
+		{
+			desc:   "valid cert",
+			certs:  validCert,
+			wanted: tls.Certificate{Certificate: [][]byte{byteArr.Bytes}},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			got := getCertChain(tC.certs)
+			if !reflect.DeepEqual(got, tC.wanted) {
+				t.Errorf("wanted %v, got %v", tC.wanted, got)
 			}
 		})
 	}
